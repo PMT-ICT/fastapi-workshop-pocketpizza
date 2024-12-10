f3ca93768ad6c90598c1cf893cf1e9c39440efa5
Jorard <jorard@pm.me>
2024-12-10 13:33:38 +0100

    finished exercise app for now

--- src/pocketpizza/api/order.py
+++ src/pocketpizza/api/order.py
@@ -1,9 +1,7 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
-from pocketpizza.schemas import CreateOrder, Order
 from pocketpizza.repositories import order
-from pocketpizza.dependencies.user import CurrentUser
 
 router = APIRouter(tags=["order"])
 
@@ -15,29 +13,12 @@ OrderRepositoryDependency = Annotated[
 
 # TODO: Exercise 1.2
 # TODO: Exercise 2.2
-@router.get("/order")
-def get_orders(
-    # TODO: create a dependency for the OrderRepository class and use it here
-    order_repository: OrderRepositoryDependency,
-    current_user: CurrentUser
-    
-) -> list[Order]:
-    # TODO: return list of orders from the order repository
-    return order_repository.get_orders(current_user.user_id)
-
+...
 
 # TODO: Exercise 1.3
 # TODO: Exercise 2.3
-@router.get("/order/{order_id}")
-def get_order_by_id(order_id: int, order_repository: OrderRepositoryDependency):
-    order = order_repository.get_order_by_id(order_id)
-
-    if order is None:
-        raise HTTPException(status_code=404)
-
+...
 
 # TODO: Exercise 1.4
 # TODO: Exercise 2.3
-@router.post("/order")
-def create_order(order: CreateOrder, order_repository: OrderRepositoryDependency):
-    return order_repository.create_order(order)
+...
--- src/pocketpizza/main.py
+++ src/pocketpizza/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
-from pocketpizza.api import order, pizza, user
+from pocketpizza.api import order, user
 from pocketpizza.lifespan import lifespan
 
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(order.router)
-app.include_router(pizza.router)
 
 # TODO: uncomment for Exercise 2
-app.include_router(user.router)
+# app.include_router(user.router)
+
+# TODO: uncomment for Bonus Exercise
+# from pocketpizza.api import pizza
+# app.include_router(pizza.router)
